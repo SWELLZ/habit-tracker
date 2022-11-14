9d5039e3b2d881f965bd52436c83e8cb917f0744
SWELLZ <microsoftdavid4@gmail.com>
2022-11-14 15:10:37 -0800

    fix tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 
 function App() {
-  return null
+  return (
+    <div>
+      <h1 className="text-lg text-red-600">Hello</h1>
+    </div>
+  )
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./stc/**/*/.{js,jsx}"
+    "./src/**/*.{js,jsx}"
   ],
   theme: {
     extend: {},
