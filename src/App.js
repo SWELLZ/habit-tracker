d0263a712a712782849f08bffeea3533c41e04ad
SWELLZ <microsoftdavid4@gmail.com>
2022-11-14 15:36:27 -0800

    add better sign up styling

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 
 function App() {
   return (
-    <div className="min-h-screen grid place-items-center">
+    <div className="min-h-screen grid place-items-center bg-amber-50">
       <form
-        className="bg-yellow-200 flex flex-col items-center w-[500px] h-[500px] justify-evenly"
+        className="bg-amber-200 rounded-md shadow-lg flex flex-col items-center w-[500px] h-[500px] justify-evenly"
         onSubmit={(e) => e.preventDefault()}
       >
         <h2 className="text-2xl font-bold">Sign Up!</h2>
 
         <label className="w-4/5">
           <p>Email:</p>
-          <input className="w-full" type="email" required />
+          <input className="w-full bg-amber-50 py-1 px-1 rounded-sm shadow-sm" type="email" required />
         </label>
 
         <label className="w-4/5">
           <p>Password:</p>
-          <input className="w-full" type="password" required />
+          <input className="w-full bg-amber-50 py-1 px-1 rounded-sm shadow-sm" type="password" required />
         </label>
 
         <label className="w-4/5">
           <p>Confirm Password:</p>
-          <input className="w-full" type="password" required />
+          <input className="w-full bg-amber-50 py-1 px-1 rounded-sm shadow-sm" type="password" required />
         </label>
 
-        <div className="w-4/5 flex gap-4 items-center">
+        <div className="w-4/5 flex flex-col gap-2 items-center">
           <button
-            className="py-2 bg-white px-4 rounded-sm shadow"
+            className="py-2 w-full bg-[#6ADBA6] px-4 rounded-sm shadow"
             type="submit"
           >
             Sign Up
